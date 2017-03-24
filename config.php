ff9dc9cb8dd8ab3a93c4bd77cffc2aecdc4ad39c
Benjamin Spriggs <benjspriggs@sprico.com>
2017-03-23 20:52:12 -0700

    add spyc to config

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 require_once "./lib/spyc/Spyc.php";
 
 // Read settings from the config
-$config_filename = "app.config.yml";
+$config_filename = "./app.config.yml";
 $config = spyc_load_file($config_file);
 
 ?>
